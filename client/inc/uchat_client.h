cf2f4108557113e5b636062257295ff746d16023
Vitalii <a1vitalii.sv@gmail.com>
2020-11-18 14:54:01 +0200

    ...

--- client/inc/uchat_client.h
+++ client/inc/uchat_client.h
@@ -1,8 +1,11 @@
 #ifndef UCHAT_CLIENT
 #define UCHAT_CLIENT
+
 #include <gtk/gtk.h>
 
 #define WIN_WIDTH_MIN 715
 #define WIN_HEIGHT_MIN 480
+
+GdkPixbuf *create_pixbuf(const gchar *filename);
 
 #endif
--- client/src/main.c
+++ client/src/main.c
@@ -3,14 +3,18 @@
 static void on_activate (GtkApplication *app) {
   // Create a new window
   GtkWidget *window = gtk_application_window_new (app);
+  GdkPixbuf *icon = create_pixbuf("client/logo.png");
   
   //Configure the window
   gtk_window_set_title(GTK_WINDOW(window), "UchatApp");
   gtk_window_set_default_size(GTK_WINDOW(window), WIN_WIDTH_MIN, WIN_HEIGHT_MIN);
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_window_set_icon(GTK_WINDOW(window), icon);
+  
   gtk_widget_show (window);
   
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_close), NULL);
+  g_object_unref(icon);
 }
 
 int main (int argc, char *argv[]) {
