6dcb4647b5072ca6fd5064fab7c09117b481b2ff
Vitalii <a1vitalii.sv@gmail.com>
2020-11-22 15:12:40 +0200

    Found out how to draw images

--- client/src/main.c
+++ client/src/main.c
@@ -1,20 +1,19 @@
 #include "../inc/uchat_client.h"
 
-/*
-void do_drawing(cairo_t *cr) {
-    cairo_set_source_surface(cr, t_settings.standard, 38, 48);
-    cairo_paint(cr);
-}
-
 gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
-    do_drawing(cr);
+    cairo_set_source_surface(cr, t_settings.standard, 0, 0);
+    cairo_paint(cr);
     if (widget) {}
     if (user_data) {}
     return FALSE;
-} */
+} 
 
 int main(int argc, char *argv[]) {
   GtkWidget *window;
+  GtkWidget *main_area;
+  
+  GtkWidget *settings;
+  t_settings.standard = cairo_image_surface_create_from_png("client/img/settings-standard.png");
   
   gtk_init(&argc, &argv);
   
@@ -25,31 +24,25 @@ int main(int argc, char *argv[]) {
   mx_init_window(window, icon);
 
   // Create a main area where all widgets will be shown
-  GtkWidget *main_area = gtk_fixed_new();
+  main_area = gtk_fixed_new();
   gtk_container_add(GTK_CONTAINER(window), main_area);
+  gtk_widget_set_size_request(GTK_WIDGET(main_area), CUR_WIDTH, CUR_HEIGHT);
   
-  // Create the right area
-  GtkWidget *right_field = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-  //gtk_container_add(GTK_CONTAINER(main_area), right_field);
-  gtk_fixed_put(GTK_FIXED(main_area), right_field, 0, 0);
+  settings = gtk_drawing_area_new();
+  gtk_fixed_put(GTK_FIXED(main_area), settings, 100, 0);
+  gtk_widget_set_size_request(GTK_WIDGET(settings), CUR_WIDTH, CUR_HEIGHT);
   
-  /*
-  // Create the left area
-  GtkWidget *left_area = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-  gtk_container_add(GTK_CONTAINER(main_area), left_area);*/
-
-  // Create a background
-  GtkWidget *background = gtk_drawing_area_new();
-  gtk_fixed_put(GTK_FIXED(main_area), background, 0, 0);
-  gtk_widget_set_size_request(background, CUR_WIDTH, CUR_HEIGHT);
-  g_signal_connect(G_OBJECT(background), "draw", G_CALLBACK(mx_on_draw_event_background), NULL);
+  g_signal_connect(G_OBJECT(settings), "draw",
+                   G_CALLBACK(on_draw_event), NULL);
+  
 
   gtk_widget_show_all (window);
+  gtk_widget_show(main_area);
   
   gtk_main();
   
   g_object_unref(icon); //in fact, a "free" command
-
+  cairo_surface_destroy(t_settings.standard);
   return 0;
 }
 
