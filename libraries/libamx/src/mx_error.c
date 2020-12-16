78227492bef576257ae5bda5cb218fb9ead3d3f5
Ihor Antypin <iantypin@c1r3p5.khpi.ucode-connect.study>
2020-12-16 16:55:48 +0200

    changes

--- libraries/libamx/src/mx_error.c
+++
@@ -1,8 +0,0 @@
-#include "../inc/libmx.h"
-
-void mx_error(char *msg, int status) {
-    if (status < 0) {
-        perror(msg);
-        exit(1);
-    }
-}
--- libraries/libamx/src/mx_strlen.c
+++
@@ -1,9 +0,0 @@
-#include "../inc/libmx.h"
-
-int mx_strlen(const char *s) {
-	int c = 0;
-	
-	while (s[c])
-		c++;
-	return c;
-}
