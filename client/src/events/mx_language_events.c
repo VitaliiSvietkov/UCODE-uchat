6922ef944ff6b205c3ea73f55707dcd077090963
VitaliiSvietkov <a1vitalii.sv@gmail.com>
2020-12-18 11:25:39 +0200

    Small fix

--- client/src/events/mx_language_events.c
+++ client/src/events/mx_language_events.c
@@ -9,6 +9,7 @@ void mx_language_eventbox_click(GtkWidget *widget, GdkEventButton *event, char *
         language = 2;
     mx_edit_language(language);
     mx_get_language_arr();
+    gtk_entry_set_text(GTK_ENTRY(entry_search), text_for_labels[13]);
     t_labels *temp = labels_head->next;
     while (temp != NULL) {
         gtk_label_set_text(GTK_LABEL(temp->data), text_for_labels[temp->index]);
