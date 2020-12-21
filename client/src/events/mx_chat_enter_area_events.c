bd0e0721d18cdbe93771abe13ff0b380e4eae095
VitaliiSvietkov <a1vitalii.sv@gmail.com>
2020-12-21 17:34:54 +0200

    Some additional features in the 'more' widget

--- client/src/events/mx_chat_enter_area_events.c
+++ client/src/events/mx_chat_enter_area_events.c
@@ -102,6 +102,25 @@ void mx_send_message(GtkWidget *widget, GdkEventButton *event, GtkWidget *entry)
 
 // More button
 //=================================================================================
+void more_content_click(GtkWidget *widget, GdkEventButton *event, GtkWidget *data) {
+    if (event->type == GDK_BUTTON_PRESS && event->button == 1) {
+        GtkStateFlags flags = gtk_widget_get_state_flags(GTK_WIDGET(widget));
+        if (!(flags & GTK_STATE_FLAG_CHECKED)) {
+
+            flags = gtk_widget_get_state_flags(GTK_WIDGET(gtk_grid_get_child_at(GTK_GRID(data), 1, 1)));
+            if (flags & GTK_STATE_FLAG_CHECKED) {
+                gtk_widget_unset_state_flags(GTK_WIDGET(gtk_grid_get_child_at(GTK_GRID(data), 1, 1)), GTK_STATE_FLAG_CHECKED);
+            }
+            else {
+                gtk_widget_unset_state_flags(GTK_WIDGET(gtk_grid_get_child_at(GTK_GRID(data), 3, 1)), GTK_STATE_FLAG_CHECKED);
+            }
+
+            gtk_widget_set_state_flags(GTK_WIDGET(widget), GTK_STATE_FLAG_CHECKED, TRUE);
+
+        }
+    }
+}
+
 void mx_more_click(GtkWidget *widget, GdkEventButton *event, t_img_button *data) {
     if (event->type == GDK_BUTTON_PRESS && event->button == 1) {
         if (!(data->active)) {
@@ -112,11 +131,31 @@ void mx_more_click(GtkWidget *widget, GdkEventButton *event, t_img_button *data)
             gtk_widget_set_name(GTK_WIDGET(more_box), "more_box");
             GtkAllocation alloc;
             gtk_widget_get_allocation(GTK_WIDGET(widget), &alloc);
-            gtk_fixed_put(GTK_FIXED(chat_area), more_box, alloc.x - 260, alloc.y - 515);
+            gtk_fixed_put(GTK_FIXED(chat_area), more_box, alloc.x - 265, alloc.y - 515);
 
+            GtkWidget *more_grid = gtk_grid_new();
+            gtk_container_add(GTK_CONTAINER(more_box), more_grid);
+            gtk_widget_set_valign(GTK_WIDGET(more_grid), GTK_ALIGN_CENTER);
+            gtk_widget_set_size_request(GTK_WIDGET(more_grid), 300, 480);
+            gtk_widget_set_margin_top(GTK_WIDGET(more_grid), 10);
+            gtk_widget_set_margin_bottom(GTK_WIDGET(more_grid), 10);
+
+            GtkWidget *stickers_eventbox = gtk_event_box_new();
+            gtk_widget_set_size_request(GTK_WIDGET(stickers_eventbox), 150, 45);
+            gtk_widget_set_name(GTK_WIDGET(stickers_eventbox), "stickers_eventbox");
+            gtk_widget_set_state_flags(GTK_WIDGET(stickers_eventbox), GTK_STATE_FLAG_CHECKED, TRUE);
+            g_signal_connect(G_OBJECT(stickers_eventbox), "button_press_event",
+                G_CALLBACK(more_content_click), more_grid);
+            gtk_grid_attach(GTK_GRID(more_grid), stickers_eventbox, 1, 1, 2, 1);
+
+            GtkWidget *gifs_eventbox = gtk_event_box_new();
+            gtk_widget_set_size_request(GTK_WIDGET(gifs_eventbox), 150, 45);
+            gtk_widget_set_name(GTK_WIDGET(gifs_eventbox), "gifs_eventbox");
+            g_signal_connect(G_OBJECT(gifs_eventbox), "button_press_event",
+                G_CALLBACK(more_content_click), more_grid);
+            gtk_grid_attach(GTK_GRID(more_grid), gifs_eventbox, 3, 1, 1, 1);
             
-            gtk_widget_set_size_request(GTK_WIDGET(more_box), 300, 500);
-
+            
             gtk_widget_show_all(GTK_WIDGET(more_box));
         }
         else {
