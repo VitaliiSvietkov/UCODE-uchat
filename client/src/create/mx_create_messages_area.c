dc84bd892c3ddcc0ff11973067fd93016d544005
VitaliiSvietkov <a1vitalii.sv@gmail.com>
2021-01-18 20:29:31 +0200

    Implemented show of the day of messages sending

--- client/src/create/mx_create_messages_area.c
+++ client/src/create/mx_create_messages_area.c
@@ -9,6 +9,7 @@ void mx_create_messages_area(void) {
     gtk_widget_set_name(GTK_WIDGET(right_container), "chat_background");
     gtk_widget_set_size_request(GTK_WIDGET(messages_box), CUR_WIDTH - L_FIELD_WIDTH, CUR_HEIGHT - 50);
     gtk_container_add(GTK_CONTAINER(right_container), messages_box);
+
     sqlite3 *db = mx_opening_db();
     t_message *msg = NULL;
     sqlite3_stmt *res;
@@ -21,14 +22,19 @@ void mx_create_messages_area(void) {
             curr_destination, t_user.id, curr_destination, t_user.id);
     sqlite3_prepare_v2(db, sql, -1, &res, 0);
     while (sqlite3_step(res) != SQLITE_DONE) {
-        msg = mx_push_back_message(&curr_room_msg_head, 
+        mx_push_back_message(&curr_room_msg_head, 
             mx_strdup((char *)sqlite3_column_text(res, 2)), 
             (unsigned int)sqlite3_column_int64(res, 1),
             mx_read_image_message((unsigned int)sqlite3_column_int64(res, 0), db),
             (time_t)sqlite3_column_int64(res, 3));
-        mx_add_message(messages_box, msg);
     }
     sqlite3_finalize(res);
     sqlite3_close(db);
+
+    msg = curr_room_msg_head;
+    while (msg != NULL) {
+        mx_add_message(messages_box, msg);
+        msg = msg->next;
+    }
     gtk_widget_show_all(GTK_WIDGET(right_container));
 }
--- client/src/mx_add_message.c
+++ client/src/mx_add_message.c
@@ -2,16 +2,29 @@
 
 void mx_add_message(GtkWidget *container, t_message *message) {
     GtkWidget *msg = mx_create_message(message);
+    GtkWidget *day_show = NULL;
 
-    if (message->next != NULL && message->next->seconds - message->seconds >= 86400) {
+
+    if (message->id == 1) {
         char *day_info = mx_strndup(ctime(&message->seconds) + 4, 7);
         day_info = mx_strjoin(day_info, ctime(&message->seconds) + 20);
-        GtkWidget *day_show = gtk_label_new(day_info);
+        GtkWidget *first_date = gtk_label_new(day_info);
+        free(day_info);
+        gtk_widget_set_name(GTK_WIDGET(first_date), "day_info_show");
+        gtk_widget_set_halign(GTK_WIDGET(first_date), GTK_ALIGN_CENTER);
+        gtk_widget_set_margin_top(GTK_WIDGET(first_date), 5);
+        gtk_box_pack_start(GTK_BOX(container), first_date, FALSE, FALSE, 0);
+    }
+    if (message->next != NULL && message->next->seconds - message->seconds >= 86400) {
+        char *day_info = mx_strndup(ctime(&message->next->seconds) + 4, 7);
+        day_info = mx_strjoin(day_info, ctime(&message->next->seconds) + 20);
+        day_show = gtk_label_new(day_info);
         free(day_info);
         gtk_widget_set_name(GTK_WIDGET(day_show), "day_info_show");
         gtk_widget_set_halign(GTK_WIDGET(day_show), GTK_ALIGN_CENTER);
-        gtk_box_pack_start(GTK_BOX(container), day_show, FALSE, FALSE, 0); 
+        gtk_widget_set_margin_top(GTK_WIDGET(day_show), 5); 
     }
+
     if (message->uid == (unsigned int)t_user.id) {
         gtk_widget_set_halign(GTK_WIDGET(msg), GTK_ALIGN_END);
         gtk_widget_set_margin_end(GTK_WIDGET(msg), 7);
@@ -24,5 +37,10 @@ void mx_add_message(GtkWidget *container, t_message *message) {
     gtk_widget_set_margin_top(GTK_WIDGET(msg), 5);
 
     gtk_box_pack_start(GTK_BOX(container), msg, FALSE, FALSE, 0); 
+
+    if (message->next != NULL && message->next->seconds - message->seconds >= 86400
+        && day_show != NULL)
+        gtk_box_pack_start(GTK_BOX(container), day_show, FALSE, FALSE, 0);
+
     gtk_widget_show_all(GTK_WIDGET(msg));
 }
