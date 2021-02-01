633ecbe2d3d63293bd60eb0f7e0380900eb33a08
Vladislav Veklenko <vveklenko@c1r7p1.khpi.ucode-connect.study>
2021-02-01 16:23:17 +0200

    fdfsds

--- server/src/mx_listening_socket.c
+++ server/src/mx_listening_socket.c
@@ -14,12 +14,12 @@ int mx_listening_socket(int port) {
     mx_error("Creating socket error", socketfd);
     struct sockaddr_in serv_addr = serv_address_description(port);
     int flag = 1;  
-    /*if (-1 == setsockopt(socketfd, SOL_SOCKET, SO_KEEPALIVE, &flag, sizeof(flag))) {  
-        perror("setsockopt fail");
-    }*/
-    if (-1 == setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag))) {  
+    if (-1 == setsockopt(socketfd, SOL_SOCKET, SO_KEEPALIVE, &flag, sizeof(flag))) {  
         perror("setsockopt fail");
     }
+    /*if (-1 == setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag))) {  
+        perror("setsockopt fail");
+    }*/
     int bind_status = bind(socketfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     mx_error("Bind error", bind_status);
     return socketfd;
