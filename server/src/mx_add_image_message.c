3d9297da148f6bfc8c277346e5363744bb67a24f
Vitalii Svietkov <vsvietkov@c1r4p3.khpi.ucode-connect.study>
2021-02-04 12:59:01 +0200

    Small fix of updating image in message

--- server/src/mx_add_image_message.c
+++ server/src/mx_add_image_message.c
@@ -27,6 +27,7 @@ void mx_add_image_message(char **data, int sockfd) {
     unsigned int flen = b64d_size(out_size);
     unsigned char *decoded = malloc( (sizeof(char) * out_size) + 1);
     flen = b64_decode(encoded, out_size, decoded);
+    free(encoded);
 
     fwrite(decoded, flen, 1, fp);
     if (ferror(fp))
@@ -35,41 +36,12 @@ void mx_add_image_message(char **data, int sockfd) {
     int r = fclose(fp);
     if (r == EOF)
         fprintf(stderr, "Cannot close file handler\n");
-    
-    free(encoded);
-    free(decoded);
-
-    // Read from the file and add to db
-    fp = fopen("server/data/tmp_msg_image.png", "rb");
-    if (fp == NULL)
-        fprintf(stderr, "Cannot open image file\n");    
-
-    fseek(fp, 0, SEEK_END);
-    if (ferror(fp)) {
-        fprintf(stderr, "fseek() failed\n");
-        r = fclose(fp);
-        if (r == EOF)
-            fprintf(stderr, "Cannot close file handler\n");          
-    }  
-
-    char read_data[flen + 1];
-    int size = fread(read_data, flen, 1, fp);
-    if (ferror(fp)) {
-        fprintf(stderr, "fread() failed\n");
-        r = fclose(fp);
-        if (r == EOF)
-            fprintf(stderr, "Cannot close file handler\n");
-    }
-
-    r = fclose(fp);
-    if (r == EOF)
-        fprintf(stderr, "Cannot close file handler\n");
 
     sqlite3 *db = mx_opening_db();
     sqlite3_stmt *pStmt;
     char sql[flen + 250];
     bzero(sql, flen + 250);
-    sprintf(sql, "UPDATE Messages SET Image=? WHERE id=%d AND\
+    sprintf(sql, "UPDATE Messages SET Image = ? WHERE id=%d AND\
             ((addresser=%d OR addresser=%d) AND (destination=%d OR destination=%d));",
             m_id, uid, dst, uid, dst);
     
@@ -77,7 +49,7 @@ void mx_add_image_message(char **data, int sockfd) {
     if (rc != SQLITE_OK)
         fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
 
-    sqlite3_bind_blob(pStmt, 1, read_data, size, SQLITE_STATIC);    
+    sqlite3_bind_blob(pStmt, 1, decoded, flen, SQLITE_STATIC);
     rc = sqlite3_step(pStmt);
     if (rc != SQLITE_DONE)
         printf("execution failed: %s", sqlite3_errmsg(db));
@@ -85,5 +57,6 @@ void mx_add_image_message(char **data, int sockfd) {
     sqlite3_finalize(pStmt);  
     sqlite3_close(db);
 
-    //remove("server/data/tmp_recv_avatar.png");
+    free(decoded);
+    remove("server/data/tmp_msg_image.png");
 }
